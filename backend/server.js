eb114e3b7cd7481098b40a8e0d0c2b481ae2f955
Vivek Gudipudi <vivekgudipudi@gmail.com>
2023-08-22 15:06:50 +0530

    update ip

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const server = require("http").createServer(app);
 
 const io = require("socket.io")(server, {
   cors: {
-    origin: "http://localhost:3000/",
+    origin: "http://192.168.53.60:3000/",
     credentials: true,
     methods: ["GET", "POST"],
     allowedHeaders: "Access-Control-Allow-Origin"
