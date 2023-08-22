e6a8e83e9d2a597e807732d5cb7d01dad32a80e1
Vivek Gudipudi <vivekgudipudi@gmail.com>
2023-08-22 15:19:21 +0530

    random commit

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,12 @@ const app = require("express")();
 const server = require("http").createServer(app);
 
 const io = require("socket.io")(server, {
+    allowRequest: (req, callback) => {
+
+        const noOriginHeader = req.headers.origin === undefined;
+        console.log(req.headers.origin)
+        callback(null, true); // only allow requests without 'origin' header
+      },
   cors: {
     origin: ["http://192.168.53.60:3000","*","http://localhost:3000"],
     credentials: true,
